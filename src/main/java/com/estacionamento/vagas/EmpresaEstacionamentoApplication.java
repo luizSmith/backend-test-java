32a3fb98c047f64583871e6ecb3d6f98cc0b2cb4
Luiz Victor <luiz.victor100@hotmail.com>
2020-03-30 00:11:38 -0300

    primeiro commit

---
+++ src/main/java/com/estacionamento/vagas/EmpresaEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.estacionamento.vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaEstacionamentoApplication.class, args);
+	}
+
+}
