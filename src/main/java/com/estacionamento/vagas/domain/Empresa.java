6c2cd0ff6858f0a5a2c8fd4755237adbb1ed210f
Luiz Victor <luiz.victor100@hotmail.com>
2020-03-30 00:11:47 -0300

    Empresa

---
+++ src/main/java/com/estacionamento/vagas/domain/Empresa.java
@@ -0,0 +1,69 @@
+package com.estacionamento.vagas.domain;
+
+public class Empresa {
+
+	private Integer id;
+	private String nome;
+	private String cnpj;
+	
+	
+	public Empresa() {
+	}
+
+	public Empresa(Integer id, String nome, String cnpj) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cnpj = cnpj;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empresa other = (Empresa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
