2992135fd9acdcf3f2fad893281055f4759f85ec
Luiz Victor <luiz.victor100@hotmail.com>
2020-03-30 00:28:14 -0300

    Veiculo

---
+++ src/main/java/com/estacionamento/vagas/domain/Veiculo.java
@@ -0,0 +1,88 @@
+package com.estacionamento.vagas.domain;
+
+public class Veiculo {
+	
+	private Integer id;
+	private String marca;
+	private String modelo;
+	private String cor;
+	private String placa;
+	
+	public Veiculo() {
+	}
+
+	public Veiculo(Integer id, String marca, String modelo, String cor, String placa) {
+		super();
+		this.id = id;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cor = cor;
+		this.placa = placa;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
