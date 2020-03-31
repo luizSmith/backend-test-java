202384d8c232db7082e97f856917b47521d8cbf5
Luiz Victor <luiz.victor100@hotmail.com>
2020-03-31 19:13:20 -0300

    Paginação Vagas carros e motos

--- src/main/java/com/estacionamento/vagas/resource/VagaMotoResource.java
+++ src/main/java/com/estacionamento/vagas/resource/VagaMotoResource.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -67,5 +69,25 @@ public class VagaMotoResource {
 	public ResponseEntity<List<VagaMoto>> findAll() {
 		List<VagaMoto> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	//Parametros opcionais
+	public ResponseEntity<Page<VagaMoto>> findPage(
+			// Por padrao pagina = 0
+			@RequestParam(value="page", defaultValue="0")
+			Integer page,
+			//Linhas por pagina
+			@RequestParam(value="linesPerPage", defaultValue="48")
+			Integer linesPerPage, 
+			//Ordenar por
+			@RequestParam(value="orderBy", defaultValue="statusVaga")
+			String orderBy, 
+			//Direcao
+			@RequestParam(value="direction", defaultValue="ASC")
+			String direction
+	) {
+		Page<VagaMoto> list = service.findPage(page,linesPerPage,orderBy,direction);
+		return ResponseEntity.ok().body(list); //ele vai responder de acordo com o obj de resposta entity
 	}
 }
